5490464f2ef1ddd3509c8752c166a72aa553dd7c
Lee Verberne <lee@blarg.org>
2011-11-20 23:29:46 -0800

    ubik/infra: work on DNS-backed InfraDB

--- lib/ubik/infra/infradns.py
+++ lib/ubik/infra/infradns.py
@@ -1,3 +1,4 @@
+"""DNS-backed InfraDB Driver"""
 
 import dns.resolver
 import logging
@@ -5,78 +6,79 @@ import os
 
 log = logging.getLogger('infra.dns')
 
-if 'RUG_RESOLV_CONF' in os.environ:
-    r = dns.resolver.Resolver(os.environ['RUG_RESOLV_CONF'])
-else:
-    r = dns.resolver.get_default_resolver()
-
-def _lookup_a(query):
-    records = []
-    log.debug("Looking up A record for '%s'" % query)
-    try:
-        for record in r.query(query, 'A').rrset:
-            records.append(record)
-    except dns.resolver.NXDOMAIN:
-        pass
-    except dns.resolver.NoAnswer:
-        pass
-    return records
-
-# takes a single name "query", performs a TXT lookup, and returns the result as
-# a list with any potential role formatting removed.
-# If to_hosts is true, this function will only valid, fully-qualified hostnames.  
-# This will also cause any sub-roles to be expanded
-def _lookup_txt_role(query, to_hosts=True):
-    records = []
-    log.debug("Looking up TXT record for '%s'" % query)
-    try:
-        answer = r.query(query, 'TXT')
-        for record in answer.rrset:
-            for txt in str(record).split():
-                # Remove spurious quotes and tags
-                host = txt.translate(None,"'\"").split(':')[-1]
-                if to_hosts:
-                    # Return FQDN
-                    fqdn = '.'.join((host,str(answer.qname.parent())))
-                    if _lookup_a(fqdn):
-                        records.append(fqdn)
-                    else:
-                        records.extend(_lookup_txt_role(fqdn))
-                else:
-                    records.append(host)
-    except dns.resolver.NXDOMAIN:
-        pass
-    except dns.resolver.NoAnswer:
-        pass
-    return records
-
-def expandhosts(records):
-    hosts = []
-    log.info("attempting to expand hostnames for: " + ' '.join(records))
-    for record in records:
-        if len(_lookup_a(record)) > 0:
-            # If this is a host, use it verbatim
-            hosts.append(record)
-        else:
-            # try to expand as txt record
-            hosts.extend(_lookup_txt_role(record))
-
-    # Dedupe hosts, but preserve order
-    # Caveat: dedupe is currently naiive because TXT records are expanded to
-    # FQDN while A records remain unqualified
-    seen = set()
-    return [x for x in hosts if x not in seen and not seen.add(x)]
-
-def listroles(domains=None):
-    if domains == None or len(domains) == 0:
-        domains = ('.',)
-    roles = {}
-    for domain in domains:
-        query = '.'.join(('services',domain)).strip('.')
-        log.info("attempting to list roles for: " + query )
-        roles[domain] = _lookup_txt_role(query, False)
-    return roles
-
 class InfraDBDriverDNS(object):
-    pass
+    """InfraDB DNS Driver class
 
+    This provides access to an InfraDB created from DNS.
+
+    """
+    def __init__(self):
+        """Initialize a new DNS InfraDB Driver
+
+        >>> idb=InfraDBDriverDNS()
+        >>>
+
+        """
+        log.debug("Initialize InfraDB DNS driver")
+        if 'RUG_RESOLV_CONF' in os.environ:
+            self.resolver = dns.resolver.Resolver(os.environ['RUG_RESOLV_CONF'])
+        else:
+            self.resolver = dns.resolver.get_default_resolver()
+
+    def lookup_host(self, query):
+        """Look up a host based on partial name, return FQDN
+
+        >>> idb=InfraDBDriverDNS()
+        >>> idb.lookup_host('balboa.sjc1')
+        u'balboa.sjc1.pontiflex.net'
+        >>> idb.lookup_host('bogus')
+        >>>
+
+        """
+        log.debug("Looking up A record for '%s'", query)
+        try:
+            answer =  self.resolver.query(query, 'A')
+        except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
+            pass
+        else:
+            if len(answer) > 0:
+                return unicode(answer.canonical_name).rstrip('.')
+        return None
+
+    def lookup_role(self, query):
+        """Resolve a role to a list of host FQDNs
+
+        >>> idb=InfraDBDriverDNS()
+        >>> idb.lookup_role('ads') #doctest: +NORMALIZE_WHITESPACE
+        [u'sprite.atl1.pontiflex.net', u'bertha.dfw1.pontiflex.net',
+         u'freehold.ewr2.pontiflex.net', u'balboa.sjc1.pontiflex.net']
+        >>> idb.lookup_role('ads.atl1')
+        [u'sprite.atl1.pontiflex.net']
+        >>> idb.lookup_role('bogus')
+        []
+        >>> idb.lookup_role('broken')
+        []
+        >>>
+
+        """
+        hosts = []
+        log.debug("Looking up TXT record for '%s'" % query)
+        try:
+            answer = self.resolver.query(query, 'TXT')
+            for record in answer.rrset:
+                for txt_sub in str(record).split():
+                    # Remove tags and spurious quotes
+                    # TODO: the following translate() is not unicode safe
+                    txt_sub = txt_sub.translate(None, "'\"").split(':')[-1]
+                    fqdn = '.'.join((txt_sub, str(answer.qname.parent())))
+                    if self.lookup_host(fqdn):
+                        hosts.append(unicode(fqdn.rstrip('.')))
+                    else:
+                        hosts.extend(self.lookup_role(fqdn))
+        except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
+            pass
+        return hosts
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS, verbose=False)
