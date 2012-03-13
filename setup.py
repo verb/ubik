71fb55a0333d5c06ff2745000fa9a3be9fb77ae8
Lee Verberne <verb@pontiflex.com>
2012-03-13 23:25:05 +0000

    configure distutils

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from distutils.core import setup
+
+setup(name='ubik',
+      version='0.10.0',
+      author='Lee Verberne',
+      author_email='lee@blarg.org',
+      description='Use only as directed.',
+      license='GPLv3',
+      package_dir={'': 'lib'},
+      packages=['ubik',
+                'ubik.fab',
+                'ubik.hats',
+                'ubik.infra',
+                'ubik.rug',
+                ],
+      requires=['fabric'],
+      url='http://github.com/verb/ubik',
+      )
