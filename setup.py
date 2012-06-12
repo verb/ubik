cf13cac2fa7803bf2eefe8d2652e5e0b2b12ae1a
Lee Verberne <verb@pontiflex.com>
2012-06-12 01:15:43 +0000

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='ubik',
-      version='0.10.0',
+      version='0.10.1',
       author='Lee Verberne',
       author_email='lee@blarg.org',
       description='Use only as directed.',
